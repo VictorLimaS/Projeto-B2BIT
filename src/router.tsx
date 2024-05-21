a1e967b45c80e4d2d31ce996f2f1a0d4036d48f7
victorlimas <victorlimasantos99@hotmail.com>
2024-05-20 22:16:00 -0300

    Uploading complete project

--- src/router.tsx
+++ src/router.tsx
@@ -1,33 +1,27 @@
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import { useEffect, useState } from 'react';
-
 
 export function Router() {
-    let token = localStorage.getItem('token');
-    const [redirect, setRedirect] = useState(false)
+    const [redirect, setRedirect] = useState(false);
 
     useEffect(() => {
+        const token = localStorage.getItem('token');
         if (!token) {
             setRedirect(false);
-        }
-        else {
+        } else {
             setRedirect(true);
         }
-    }, [token])
+    }, []);
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={redirect ? <Navigate to="/home" />:<Login />} />
-                <Route path='/home' element={
-                    redirect ? <Home /> : <Navigate to="/" />
-                } />
+                <Route path='/' element={redirect ? <Navigate to="/home" /> : <Login />} />
+                <Route path='/home' element={redirect ? <Home /> : <Navigate to="/" />} />
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
-
